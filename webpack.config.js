99b49d5b6c7039329748518373398dae52493761
xinyuan-yao <yao.ntno@gmail.com>
2020-09-16 13:17:53 +0000

    git add

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    host: "0.0.0.0",
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+
+  },
+  plugins: [new HtmlWebpackPlugin({title: "test"})]
+};
